3795660849d1d8d11a425b6a42be99c7c68734dc
Pulin Kulshrestha <pulinkul5@gmail.com>
2026-06-23 23:39:18 +0530

    test 2

--- playground/python/test.py
+++ playground/python/test.py
@@ -11,4 +11,24 @@ def add(item, bucket=None):
 
 logging.basicConfig(level=logging.INFO)
 
-print(add(1))
+# print(add(2))
+# print(add(3))
+
+
+# def add(item, bucket=[]):
+#     bucket.append(item)
+#     print("Bucket ID:", id(bucket))
+#     return bucket
+
+
+# add(1)
+
+# add(2)
+
+# add(3)
+print(add.__defaults__)
+print(id(add.__defaults__))
+
+# print(id(add.__defaults__[0]))
+# add(1)
+# print(id(add.__defaults__[0]))
