58d23ff52f55a95b3b69eefe10d2f72c8a05bfa5
Pulin Kulshrestha <pulinkul5@gmail.com>
2026-07-15 23:34:16 +0530

    oops play script

--- playground/python/oops/oops_concept.py
+++ playground/python/oops/oops_concept.py
@@ -1,1 +1,9 @@
 ## let's learn about OOPS concepts in python
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+my_car = Car("Toyota", "Camry")
+print(my_car)
